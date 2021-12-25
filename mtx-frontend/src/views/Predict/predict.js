d22a91af4cb60e827be7594fba206e081177cf1e
Adil Mohammed K <adilmohammed2000@outlook.com>
2021-12-25 13:59:00 +0530

    Baic upload page

--- mtx-frontend/src/views/Predict/predict.js
+++ mtx-frontend/src/views/Predict/predict.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
+import UploadVideo from './upload';
 
 const Results = () => {
-  return <div></div>;
+  return (
+    <div>
+      <UploadVideo></UploadVideo>
+    </div>
+  );
 };
 
 export default Results;
