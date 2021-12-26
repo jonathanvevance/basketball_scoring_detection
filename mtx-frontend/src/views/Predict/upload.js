270f0ade35ff69863d41eb20b320bf6a9fe7db54
Adil Mohammed K <adilmohammed2000@outlook.com>
2021-12-26 15:46:58 +0530

    Show filedata on upload

--- mtx-frontend/src/views/Predict/upload.js
+++ mtx-frontend/src/views/Predict/upload.js
@@ -20,105 +20,35 @@ function FileUpload() {
       console.log(res.data);
     });
   };
+  const fileDataShow = () => {
+    if (fileData) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+
+          <p>File Name: {fileData.name}</p>
+
+          <p>File Type: {fileData.type}</p>
+
+          <p>Last Modified: {fileData.lastModifiedDate.toDateString()}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
   return (
     <form onSubmit={uploadFile}>
       <input type='file' name='file' onChange={getFile} required />
       <input type='submit' name='upload' value='Upload' />
+      {fileDataShow()}
     </form>
   );
 }
 
-// class UploadVideo extends Component {
-//   state = {
-//     // Initially, no file is selected
-//     selectedFile: null,
-//   };
-
-//   // On file select (from the pop up)
-//   onFileChange = (event) => {
-//     // Update the state
-//     this.setState({ selectedFile: event.target.files[0] });
-//   };
-
-//   // On file upload (click the upload button)
-//   onFileUpload = (files) => {
-//     // Create an object of formData
-//     const formData = new FormData();
-//     console.log(files);
-
-//     // Update the formData object
-//     formData.append(
-//       'myFile',
-//       this.state.selectedFile,
-//       this.state.selectedFile.name
-//     );
-
-//     // Details of the uploaded file
-//     console.log(this.state.selectedFile);
-
-//     // Request made to the backend api
-//     // Send formData object
-//     axios
-//       .post({
-//         method: 'POST',
-//         url: 'http://localhost:5000/uploadfile',
-//         data: formData,
-//       })
-//       .then((response) => {
-//         if (response.data.success) {
-//           // File uploaded successfully
-//           console.log(response.data.message);
-//           console.log('File uploaded successfully');
-//         } else {
-//           // File upload failed
-//           console.log(response.data.message);
-//           console.log('File upload failed');
-//         }
-//       });
-//   };
-
-//   // File content to be displayed after
-//   // file upload is complete
-//   fileData = () => {
-//     if (this.state.selectedFile) {
-//       return (
-//         <div>
-//           <h2>File Details:</h2>
-
-//           <p>File Name: {this.state.selectedFile.name}</p>
-
-//           <p>File Type: {this.state.selectedFile.type}</p>
-
-//           <p>
-//             Last Modified:{' '}
-//             {this.state.selectedFile.lastModifiedDate.toDateString()}
-//           </p>
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div>
-//           <br />
-//           <h4>Choose before Pressing the Upload button</h4>
-//         </div>
-//       );
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Upload Page</h1>
-//         <h3>Required file format: .mp4</h3>
-//         <div>
-//           <input type='file' onChange={this.onFileChange} />
-//           <button onClick={this.onFileUpload}>Upload!</button>
-//         </div>
-//         {this.fileData()}
-//       </div>
-//     );
-//   }
-// }
-
-// export default UploadVideo;
 export default FileUpload;
