eb2b6f61c4eb9e35044237ba17f39c9d56d39e13
Adil Mohammed K <adilmohammed2000@outlook.com>
2021-12-28 12:46:06 +0530

    Styling for upload page

--- mtx-frontend/src/views/Predict/upload.js
+++ mtx-frontend/src/views/Predict/upload.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react';
 import axios from 'axios';
 import Dropzone from 'react-dropzone-uploader';
 import 'react-dropzone-uploader/dist/styles.css';
+import { Grid } from '@material-ui/core';
 
 function FileUpload() {
   const [fileData, setFileData] = useState('');
@@ -20,23 +21,24 @@ function FileUpload() {
     }).then((res) => {
       alert(res.data.message);
       console.log(res.data);
-      document.location.href = 'http://localhost:3000/results';
     });
   };
 
   return (
-    <Dropzone
-      // getUploadParams={() => ({ url: 'http://localhost:5000/upload' })} // specify upload params and url for your files
-      onChangeStatus={({ meta, file }, status) => {
-        getFile(file);
-      }}
-      onSubmit={(files) => {
-        // console.log(files[0].file);
-        // getFile(files);
-        uploadFile();
-      }} // e.g., submit the uploaded file URLs to your backend
-      accept='video/mp4'
-    />
+    <Grid container spacing={0} style={{ padding: '10px' }}>
+      <Grid item xs={6} className='heading'>
+        <Dropzone
+          // getUploadParams={() => ({ url: 'http://localhost:5000/upload' })} // specify upload params and url for your files
+          onChangeStatus={({ meta, file }, status) => {
+            getFile(file);
+          }}
+          onSubmit={(files) => {
+            uploadFile();
+          }} // e.g., submit the uploaded file URLs to your backend
+          accept='video/mp4'
+        />
+      </Grid>
+    </Grid>
   );
 }
 
