cf35933fb7a3a494af9ddff461f7ff923e344d11
Adil Mohammed K <adilmohammed2000@outlook.com>
2021-12-28 12:47:14 +0530

    get results from server

--- mtx-frontend/src/views/Results/results.js
+++ mtx-frontend/src/views/Results/results.js
@@ -2,29 +2,7 @@ import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
 import BarChart from './barChart';
 import './results.css';
-
-const barData = [
-  { name: 'E', value: 0.12702, title: 'Graph 1' },
-  { name: 'T', value: 0.09056 },
-  { name: 'A', value: 0.08167 },
-  { name: 'O', value: 0.07507 },
-  { name: 'I', value: 0.06966 },
-  { name: 'N', value: 0.06749 },
-  { name: 'S', value: 0.06327 },
-  { name: 'H', value: 0.06094 },
-  { name: 'R', value: 0.05987 },
-  { name: 'D', value: 0.04253 },
-  { name: 'L', value: 0.04025 },
-  { name: 'C', value: 0.02782 },
-  { name: 'U', value: 0.02758 },
-  { name: 'M', value: 0.02406 },
-  { name: 'W', value: 0.0236 },
-  { name: 'F', value: 0.02288 },
-  { name: 'G', value: 0.02015 },
-  { name: 'Y', value: 0.01974 },
-  { name: 'P', value: 0.01929 },
-  { name: 'B', value: 0.01492 },
-];
+import axios from 'axios';
 
 class Results extends Component {
   constructor(props) {
@@ -33,17 +11,21 @@ class Results extends Component {
     // this.upload = this.upload.bind(this);
 
     this.state = {
-      barData: barData,
-      pieData: [],
+      barData: [],
       graphOn: false,
-      currentFile: undefined,
-      previewImage: undefined,
-      progress: 0,
 
       message: '',
       isError: false,
       loader: true,
     };
+    const barData = [];
+  }
+  componentDidMount() {
+    axios.get('http://localhost:5000/getprobab').then((result) => {
+      console.log(result.data);
+      // this.setState({ barData: result.data });
+      this.barData = result.data.data;
+    });
   }
   render() {
     return (
@@ -58,7 +40,7 @@ class Results extends Component {
         <div className='bottom'>
           <div className='centered'>
             <div className='grid-layout'>
-              <BarChart barData={barData} />
+              {/* <BarChart barData={this.barData} /> */}
             </div>
           </div>
         </div>
