6c9cfc305f263f43172bb44004a961affbd3d53a
Adil Mohammed K <adilmohammed2000@outlook.com>
2022-01-06 15:48:59 +0530

    integrate frontend with predict() using server.py

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,5 @@
-
 import os
+import csv
 import torch
 from torchvision import transforms
 
@@ -8,32 +8,36 @@ from utils.img_video_utils import save_frames_from_video_inference
 from utils.eval_utils import load_model_clf
 
 RESIZE = 128
-THRESHOLD = 0.8
-MODEL_WEIGHTS_PATH = 'models/best.pt'
-FRAMES_UPLOAD_DIRECTORY = 'data/inference/frames_upload'
+THRESHOLD = 0.93
+MODEL_WEIGHTS_PATH = "models/best.pt"
+FRAMES_UPLOAD_DIRECTORY = "data/inference/frames_upload"
+FRAMES_PROBAB_CSV_PATH = "reports/probability_values.csv"
+
+#! TODO: CODE DATA LOADER FOR INFERENCE (REQUEST BATCH SIZE AS ENV VAR)
+
 
 def run_predictions(pil_images):
 
     is_scoring = False
-    frame_probabs = []
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    frame_probabs = [["time", "values"]]
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
 
     # load model
     model = load_model_clf(MODEL_WEIGHTS_PATH, device)
     model = model.eval()
 
     # run predictions, return predictions
-    for i, pil_image in enumerate(pil_images):
+    for frame_num, pil_image in enumerate(pil_images):
 
-        if pil_image is None: # no basket detected
-            frame_probabs.append(0)
+        if pil_image is None:  # no basket detected
+            frame_probabs.append([frame_num, 0])
             continue
 
         pil_image = transforms.Resize((RESIZE, RESIZE))(pil_image)
         image_tensor = transforms.ToTensor()(pil_image)
         output = model(torch.unsqueeze(image_tensor, 0))
-        print(i, output)
-        frame_probabs.append(output)
+        frame_probabs.append([frame_num, output[0].item()])
 
         if output > THRESHOLD:
             is_scoring = True
@@ -42,17 +46,17 @@ def run_predictions(pil_images):
 
 
 def predict():
-    '''predict Yes or No per frame per video.'''
+    """predict Yes or No per frame per video."""
 
     # get frames from video
     save_frames_from_video_inference()
 
     # run basket detection
     commands = [
-        'cd ./src/yolov3_helper',
-        'sudo bash predict.sh' + ' ../../../' + FRAMES_UPLOAD_DIRECTORY + ' ../../../' + FRAMES_UPLOAD_DIRECTORY
+        "cd ./src/yolov3_helper",
+        "sudo bash predict.sh" + " ../../../" + FRAMES_UPLOAD_DIRECTORY + " ../../../" + FRAMES_UPLOAD_DIRECTORY,
     ]
-    os.system(';'.join(commands)) # bounding boxes saved to src/yolov3_helper/yolov3/output/bounding_boxes.json
+    os.system(";".join(commands))  # bounding boxes saved to src/yolov3_helper/yolov3/output/bounding_boxes.json
 
     # crop basket images
     pil_images = get_cropped_pil_images_inference()
@@ -60,6 +64,14 @@ def predict():
     # run predictions
     is_scoring, pred_probabs = run_predictions(pil_images)
 
-    return is_scoring, pred_probabs
+    # save as csv file
+    with open(FRAMES_PROBAB_CSV_PATH, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(pred_probabs)
 
-predict()
+    return is_scoring
+
+    #! TODO: SEE PROBABS FROM SAVED_MODEL_6.PT - are they good?
+
+
+# predict()
---
+++ src/server/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const request = require('request');
+const app = express();
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(
+      null,
+      '/home/amark/Projects/MTX-HackOlympics/data/inference/video_upload/'
+    );
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, 'video.mp4');
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == 'video/mp4') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('File type not accepted (.mp4 only)'));
+    }
+  },
+});
+
+function newVideo(word) {
+  return new Promise(function (fulfill, reject) {
+    request.post(
+      'http://127.0.0.1:4000/fileupload',
+      {
+        form: {
+          word: word,
+        },
+      },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          fulfill(JSON.parse(body));
+        } else {
+          reject(error, response);
+        }
+      }
+    );
+  });
+}
+
+function getProbab(name) {
+  return new Promise(function (fulfill, reject) {
+    request.post(
+      'http://localhost:4000/getvalue',
+      {
+        form: { filename: name },
+      },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          fulfill(JSON.parse(body));
+        } else {
+          reject(error, response);
+        }
+      }
+    );
+  });
+}
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (req.file) {
+      res.send({
+        status: true,
+        message: 'File Uploaded!',
+        data: await newVideo(req.file.filename),
+      });
+    } else {
+      res.status(400).send({
+        status: false,
+        data: 'File Not Found :(',
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/getprobab', async (req, res) => {
+  try {
+    res.send({
+      status: true,
+      data: await getProbab(req.name),
+      title: 'Probabilty graph',
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(5000, () => console.log('Server Running...'));
